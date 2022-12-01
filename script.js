71961fc7fb0adf6f9ab77fb2bfac6517fc3f73bb
PradeepV0 <pradeepv951@gmail.com>
2022-12-01 06:02:40 +0530

    DAY 7

---
+++ script.js
@@ -0,0 +1,24 @@
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all",true)
+request.send()
+ request.onload=function(){
+    var data = request.response
+    var result = JSON.parse(data)
+     var res = result.filter((ele)=>ele.continents=="Asia")
+     //to display the filtered result countries
+     
+     console.log(res);
+    }
+
+
+//     var request = new XMLHttpRequest();
+// request.open("GET","https://restcountries.com/v3.1/all",true)
+// request.send()
+//  request.onload=function(){
+//     var data = request.response
+//     var result = JSON.parse(data)
+//      var res = result.filter((ele)=>ele.population<300000)
+//      //to display the filtered result countries
+//      var res1 = res.map((ele)=>`${ele.population}: ${ele.name.common}`)
+//      console.log(res1)
+//     }
